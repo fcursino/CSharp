59502d840d3c6c87d20ed37bbc3482b43e13af8a
Fabricio <51213785+fcursino@users.noreply.github.com>
2020-05-03 17:41:24 -0300

    Add files via upload

---
+++ Exception Par ou Impar (MG)/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exception_Par_ou_Impar__MG_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            r = a + b;
+            try
+            {
+                if (r % 2 == 0)
+                {
+                    MessageBox.Show("O numero é par: " + r);
+                }
+                else
+                {
+                    MessageBox.Show("O numero é impar: " + r);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
